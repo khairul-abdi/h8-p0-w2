cbec7ec9b0c65ad6fe32f1120c0bb64875515e21
khairul-abdi <kabdi384@gmail.com>
2019-08-01 17:32:39 +0700

    revisi exercise-6

--- exercise-6.js
+++ exercise-6.js
@@ -4,19 +4,19 @@ console.log('==========================================');
 console.log('');
 console.log('LOOPING PERTAMA');
 
-var w=2;
+var i=2;
 
-while(w<=20){
-    console.log(w+' - I love coding');
-    w++;
+while(i<=20){
+    console.log(i+' - I love coding');
+    i++;
 }
 
 console.log('LOOPING KEDUA');
-var y=20;
+var i=20;
 
-while(y>=2){
-    console.log(y+' - I will become fullstack developer');
-    y--;
+while(i>=2){
+    console.log(i+' - I will become fullstack developer');
+    i--;
 }
 
 
@@ -31,14 +31,14 @@ console.log('==========================================');
 
 console.log('LOOPING PERTAMA');
 
-for(var a=1; a<=20; a++){
-    console.log(a+' - I love coding');
+for(var i=1; i<=20; i++){
+    console.log(i+' - I love coding');
 }
 
 console.log('LOOPING KEDUA');
 
-for(var b=20; b>=1; b--){
-    console.log(b+' - I will become fullstack developer');
+for(var i=20; i>=1; i--){
+    console.log(i+' - I will become fullstack developer');
 }
 
 
@@ -50,35 +50,33 @@ console.log('3. Angka Ganjil dan Genap');
 console.log('==========================================');
 
 
-for(var x=1; x<=100; x++){
-    if(x % 2 == 1){
-        console.log('Counter sekarang = '+x);
+for(var i=1; i<=100; i++){
+    if(i % 2 == 1){
+        console.log('Counter sekarang = '+i);
         console.log('GANJIL')   
     }else{
-        console.log('Counter sekarang = '+x);
+        console.log('Counter sekarang = '+i);
         console.log('GENAP');
         
     }
 }
 
-for(var c=1; c<=100; c+=2){
-    console.log('Counter sekarang = '+c);
-    if(c % 3 == 0){
-        console.log(c + ' KELIPATAN 3');
+
+for(var i=1; i<=100; i+=2){
+    if(i % 3 == 0){
+        console.log(i + ' KELIPATAN 3');
     }
 }
 
-for(var d=1; d<=100; d+=5){
-    console.log('Counter sekarang = '+d);
-    if(d % 6 == 0){
-        console.log(d + ' KELIPATAN 6');
+for(var i=1; i<=100; i+=5){
+    if(i % 6 == 0){
+        console.log(i + ' KELIPATAN 6');
     }
 }
 
-for(var e=1; e<=100; e+=9){
-    console.log('Counter sekarang = '+e);
-    if(e % 10 == 0){
-        console.log(e + ' KELIPATAN 10');
+for(var i=1; i<=100; i+=9){
+    if(i % 10 == 0){
+        console.log(i + ' KELIPATAN 10');
     }
 }
 
